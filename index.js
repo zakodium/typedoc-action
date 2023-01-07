f976aba5b3d7986cd896b4e7769cdd353de9afa3
Daniel Kostro <kostro.d@gmail.com>
2023-01-07 15:54:59 +0100

    feat: use katex plugin to generate mathematical formulas from readme (#13)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const exists = require('fs').existsSync;
+const { existsSync } = require('fs');
 const fs = require('fs').promises;
 const path = require('path');
 
@@ -9,6 +9,7 @@ const { exec } = require('@actions/exec');
 
 const entry = core.getInput('entry') || 'src/index.ts';
 const name = core.getInput('name');
+const cwd = process.cwd();
 
 const defaultTsconfig = `{
   "compilerOptions": {
@@ -21,26 +22,18 @@ const defaultTsconfig = `{
   const packageJson = await getPackageJson();
   const { hasTsConfig, tsConfigPath } = await ensureTsConfig();
 
-  if (!exists('node_modules')) {
+  if (!existsSync('node_modules')) {
     core.warning('node_modules is not present. Running `npm install`...');
     await exec('npm', ['install']);
   }
 
-  const args = [
-    path.join(__dirname, 'node_modules/typedoc/bin/typedoc'),
-    '--out',
-    'docs',
-    '--name',
-    name || packageJson.name,
-    '--excludePrivate',
-    '--hideGenerator',
-    '--tsconfig',
-    tsConfigPath,
-  ];
+  await writeTypedocJson(name || packageJson.name, tsConfigPath);
 
-  args.push(...entry.split(/ +/));
+  const args = [path.join(__dirname, 'node_modules/typedoc/bin/typedoc')];
 
-  await exec('node', args);
+  await exec('node', args, {
+    cwd: __dirname,
+  });
   if (!hasTsConfig) {
     await fs.unlink(tsConfigPath);
   }
@@ -48,13 +41,36 @@ const defaultTsconfig = `{
   core.setFailed(error);
 });
 
+async function writeTypedocJson(name, tsConfigPath) {
+  const options = {
+    out: path.resolve('docs'),
+    name,
+    excludePrivate: true,
+    hideGenerator: true,
+    tsconfig: path.resolve(tsConfigPath),
+    entryPoints: entry.split(/ +/).map((entry) => path.resolve(entry)),
+    // typedoc-plugin-katex plugin options
+    katex: {
+      options: {
+        delimiters: [{ left: '$', right: '$', display: true }],
+        fleqn: 'true',
+        leqno: 'false',
+      },
+    },
+  };
+  await fs.writeFile(
+    path.join(__dirname, 'typedoc.json'),
+    JSON.stringify(options, null, 2),
+  );
+}
+
 async function getPackageJson() {
   return JSON.parse(await fs.readFile('package.json', 'utf-8'));
 }
 
 async function ensureTsConfig() {
   let has = true;
-  if (!exists('tsconfig.json')) {
+  if (!existsSync('tsconfig.json')) {
     has = false;
     await fs.writeFile('tsconfig.json', defaultTsconfig);
   }
