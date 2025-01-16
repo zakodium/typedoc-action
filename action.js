ff635b6562af053d302d517128c60b2caa702e59
Michaël Zasso <targos@protonmail.com>
2025-01-16 16:20:44 +0100

    chore: add debug script

---
+++ action.js
@@ -0,0 +1,18 @@
+import core from '@actions/core';
+import {exec} from '@actions/exec';
+
+import {executeAction} from './src/index.js';
+
+const entry = core.getInput('entry') || 'src/index.ts';
+const name = core.getInput('name');
+const treatWarningsAsErrors = core.getInput('treatWarningsAsErrors') === 'true';
+
+function onWarn(message) {
+  core.warning(message)
+}
+
+try {
+  await executeAction({entry, name, treatWarningsAsErrors, onWarn, exec});
+} catch (error) {
+  core.setFailed(error);
+}
